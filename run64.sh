ab3d9df4e339f1086dcf60a9e3d2d82fd3f5d67a
William <william.o.bland08@gmail.com>
2025-08-08 15:16:08 +0100

    Added argument support to run64

--- run64.sh
+++ run64.sh
@@ -1,28 +1,49 @@
 #!/bin/bash
 
+# Assembles the assembly files it is given into object files in the same dir
+# and name as the file it was given. Links together all object files given (and
+# the ones it assembled) into a binary file in the directory and name of the
+# first argument. If given '--' as an argument, all arguments after it will be
+# passed to the executeable.
+
 if [ $# -eq 0 ]; then
 	echo "Must have at least one argument"
 	exit 1
 fi
 
 objects=()
+args=()
+is_arg=false
 
 for arg in "${@}"; do
-	file="$(pwd)/$arg"
+	if [ $is_arg = true ]; then
+		args+=($arg)
+	else
+		if [[ $arg = "--" ]]; then
+			if [[ ${#objects[@]} = 0 ]]; then
+				echo "Must give at least one file before the program arguments"
+				exit 1
+			else
+				is_arg=true
+			fi
+		else
+			file="$(pwd)/$arg"
 
-	case $file in
-		*.asm | *.s | *.S)
-			object="${file%.*}.o"
-			objects+=($object)
-			nasm -f elf64 $file -o $object || exit 1 ;;
-		*.o | *.a | *.so)
-			objects+=($file) ;;
-		*)
-			echo "'$file' does not end in a correct file extention"
-			exit 1 ;;
-	esac
+			case $file in
+				*.asm | *.s | *.S)
+					object="${file%.*}.o"
+					objects+=($object)
+					nasm -f elf64 $file -o $object || exit 1 ;;
+				*.o | *.a | *.so)
+					objects+=($file) ;;
+				*)
+					echo "'$file' does not end in a correct file extention"
+					exit 1 ;;
+			esac
+		fi
+	fi
 done
 
 output="$(pwd)/${1%.*}.bin"
 ld "${objects[@]}" -o $output || exit 1
-$output
+$output "${args[@]}"
