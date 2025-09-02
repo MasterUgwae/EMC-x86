bafb7cfb0c075183e7af82b865540bef3c5c8d1a
William <william.o.bland08@gmail.com>
2025-09-02 19:57:57 +0100

    Made run64.sh more readable

--- run64.sh
+++ run64.sh
@@ -7,8 +7,8 @@
 # passed to the executeable.
 
 if [ $# -eq 0 ]; then
-	echo "Must have at least one argument"
-	exit 1
+    echo "Must have at least one argument"
+    exit 1
 fi
 
 objects=()
@@ -16,32 +16,30 @@ args=()
 is_arg=false
 
 for arg in "${@}"; do
-	if [ $is_arg = true ]; then
-		args+=($arg)
-	else
-		if [[ $arg = "--" ]]; then
-			if [[ ${#objects[@]} = 0 ]]; then
-				echo "Must give at least one file before the program arguments"
-				exit 1
-			else
-				is_arg=true
-			fi
-		else
-			file="$(pwd)/$arg"
+    if [ $is_arg = true ]; then
+        args+=($arg)
+    elif [[ $arg = "--" ]]; then
+        if [[ ${#objects[@]} = 0 ]]; then
+            echo "Must give at least one file before the program arguments"
+            exit 1
+        else
+            is_arg=true
+        fi
+    else
+        file="$(pwd)/$arg"
 
-			case $file in
-				*.asm | *.s | *.S)
-					object="${file%.*}.o"
-					objects+=($object)
-					nasm -f elf64 $file -o $object || exit 1 ;;
-				*.o | *.a | *.so)
-					objects+=($file) ;;
-				*)
-					echo "'$file' does not end in a correct file extention"
-					exit 1 ;;
-			esac
-		fi
-	fi
+        case $file in
+            *.asm | *.s | *.S)
+                object="${file%.*}.o"
+                objects+=($object)
+                nasm -f elf64 $file -o $object || exit 1 ;;
+            *.o | *.a | *.so)
+                objects+=($file) ;;
+            *)
+                echo "'$file' does not end in a correct file extention"
+                exit 1 ;;
+        esac
+    fi
 done
 
 output="$(pwd)/${1%.*}.bin"
